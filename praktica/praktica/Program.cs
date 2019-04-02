36b86609ab50369fb9fa81ce666bc4a480d9f1d0
Максим Хохлов <maks_khokhlov_2014@mail.ru>
2019-04-02 08:19:39 +0300

    Main

--- praktica/praktica/Program.cs
+++ praktica/praktica/Program.cs
@@ -10,6 +10,11 @@ namespace praktica
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ввод данных");
+            Biblio Maks = new Biblio();
+            Maks.SetInfo();
+            Maks.GetInfo();
+            Console.ReadKey();
         }
     }
 }
